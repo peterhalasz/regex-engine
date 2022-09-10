51c8a4fbd2f1801f6b7d99eb5178d55e5b85b721
Peter Halasz <peter.halasz@live.com>
2022-09-10 21:24:53 +0200

    cleaning up

--- plotter.py
+++ plotter.py
@@ -3,7 +3,7 @@ import graphviz
 START_NODE = "Start"
 
 def plot_automaton(transition_function, starting_state, final_states):
-    g = graphviz.Digraph('G', format='png')
+    g = graphviz.Digraph('G', format='png', directory='./out')
 
     g.attr('node', shape='doublecircle')
     for final_state in final_states:
