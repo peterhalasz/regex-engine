6f02ccd02baa8d8557d7d95a274b4d468c99a8b9
Peter Halasz <peter.halasz@live.com>
2022-09-13 21:46:22 +0200

    wip - plotting dfa-nfa conversion result

--- nfa.py
+++ nfa.py
@@ -1,3 +1,4 @@
+from dfa import Dfa
 from tabulate import tabulate
 from plotter import plot_automaton
 from utils import validate_automaton, validate_symbols
@@ -93,6 +94,24 @@ class Nfa():
         headers = ['State', *[i for i in sorted(self.input_symbols)]]
         print(tabulate(sorted(table), headers=headers))
 
+
+        dfa_transition_function_2 = {}
+        for key, next_states in dfa_transition_function.items():
+            dfa_state, input_symbol = key
+            dfa_transition_function_2[",".join(dfa_state), input_symbol] = next_states
+
+        dfa_accepting_states_2 = [",".join(s) for s in dfa_accepting_states]
+
+        dfa = Dfa(
+            states=self.states,
+            input_symbols=self.input_symbols,
+            transition_function=dfa_transition_function_2,
+            starting_state=self.starting_state,
+            final_states=dfa_accepting_states_2,
+        )
+
+        return dfa
+
     def is_string_accepted(self, input_string):
         if not validate_symbols(self.input_symbols, input_string):
             print("Input string is not part of the input symbols")
@@ -119,4 +138,5 @@ if __name__ == "__main__":
     nfa = Nfa(states, input_symbols, transition_function, starting_state, final_states)
 
     #nfa.plot()
-    nfa.convert_to_dfa()
+    dfa = nfa.convert_to_dfa()
+    dfa.plot()
