c0eb1939a6a1157198c1bb4d85a73714fc4fd7b1
Peter Halasz <peter.halasz@live.com>
2022-09-17 12:15:22 +0200

    one more nfa conversion test

--- nfa_test.py
+++ nfa_test.py
@@ -76,3 +76,28 @@ class NfaTest(unittest.TestCase):
         }
 
         self.assertDictEqual(dfa.transition_function, expected_dfa_tranisition_function)
+
+    def test_convert_to_dfa_4(self):
+        states = {"A", "B", "C"}
+        input_symbols = {"0", "1"}
+        transition_function = {
+            ("A", "0"): {"A"},
+            ("A", "1"): {"A", "B"},
+            ("B", "1"): {"C"},
+        }
+        starting_state = "A"
+        final_states = {"C"}
+
+        nfa = Nfa(states, input_symbols, transition_function, starting_state, final_states)
+        dfa = nfa.convert_to_dfa()
+
+        expected_dfa_tranisition_function = {
+            ('A', '0'): 'A',
+            ('A', '1'): 'A,B',
+            ('A,B', '0'): 'A',
+            ('A,B', '1'): 'A,B,C',
+            ('A,B,C', '0'): 'A',
+            ('A,B,C', '1'): 'A,B,C',
+        }
+
+        self.assertDictEqual(dfa.transition_function, expected_dfa_tranisition_function)
