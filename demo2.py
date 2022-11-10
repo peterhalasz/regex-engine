ae1fceb3da489896bc0e5cbaf86ecb61de8ba07a
Peter Halasz <peter.halasz@live.com>
2022-11-10 23:23:55 +0100

    dfa - comments

--- demo2.py
+++ demo2.py
@@ -2,10 +2,10 @@ from regex import create_enfa_from_regex
 
 if __name__ == "__main__":
     regex = "0"
-    #regex = "0101"
-    #regex = "0+1"
-    #regex = "1*0*"
-    #regex = "0(0+1)*1"
+    # regex = "0101"
+    # regex = "0+1"
+    # regex = "1*0*"
+    # regex = "0(0+1)*1"
 
     enfa = create_enfa_from_regex(regex)
     enfa.print()
--- dfa.py
+++ dfa.py
@@ -1,24 +1,18 @@
 from plotter import plot_automaton
 from printer import print_automaton
 
-""" Deterministic Finite Automaton.
-
-A simple implementation of a dfa.
-"""
+""" Deterministic Finite Automaton."""
 
 
 class Dfa:
     def __init__(self, transition_function, starting_state, final_states):
-        # Transition function. Usually denoted by delta.
         # Here implemented as a tuple to string dict.
         # (state, input symbol) -> next state
         self.transition_function = transition_function
 
-        # Start state. Usually denoted by q0.
         self.starting_state = starting_state
 
-        # A finite set of accepting / final states. Usually denoted by F.
-        # F has to be a subset of Q.
+        # A finite set of accepting / final states.
         self.final_states = final_states
 
     def plot(self):
@@ -30,11 +24,19 @@ class Dfa:
         )
 
     def is_string_accepted(self, input_string):
+        """Check if the string is accepted by the DFA.
+
+        Args:
+            input_string: the input to check.
+
+        Returns:
+            True if the automaton accepts the string, False otherwise.
+        """
         current_state = self.starting_state
         for input_symbol in input_string:
 
             if (current_state, input_symbol) not in self.transition_function:
-                # FAILED: automaton does not terminate
+                # The automaton does not terminate
                 return False
 
             current_state = self.transition_function[(current_state, input_symbol)]
@@ -42,5 +44,5 @@ class Dfa:
         if current_state in self.final_states:
             return True
         else:
-            # Failed: automaton terminates in a non-final state
+            # The automaton terminates in a non-final state
             return False
